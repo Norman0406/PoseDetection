3a5b48bf480ec3410ab2ddd430976112221aedc1
Norman Link <norman.link@gmx.net>
2015-01-18 19:52:39 +0100

    initial particle swarm optimization algorithm

--- src/tracking/fittingmethodpso.cpp
+++ src/tracking/fittingmethodpso.cpp
@@ -3,11 +3,26 @@
 namespace pose
 {
 FittingMethodPSO::FittingMethodPSO()
+    : m_numParticles(50),
+      m_numIterations(1000),
+      m_numVariables(3),
+      m_w0(0.9f),
+      m_wt(0.4f),
+      m_maxV(2.0f),
+      m_c1(2.0f),
+      m_c2(2.0f)
 {
+    // create and initialize particles
+    m_particles.resize(m_numParticles);
+    for (size_t i = 0; i < m_particles.size(); i++)
+        m_particles[i] = new Particle(m_numVariables);
 }
 
 FittingMethodPSO::~FittingMethodPSO()
 {
+    for (size_t i = 0; i < m_particles.size(); i++)
+        delete m_particles[i];
+    m_particles.clear();
 }
 
 void FittingMethodPSO::iProcess(const cv::Mat& depthMap,
@@ -16,6 +31,68 @@ void FittingMethodPSO::iProcess(const cv::Mat& depthMap,
                                 cv::Point3f centerOfMass,
                                 const cv::Mat& projectionMatrix)
 {
+    // TODO: define the function and the actual parameters that are to be optimized.
+    // The optimization is run for the 3d root joint position and every subsequent
+    // bone that is not fixed. The energy is computed from the current bone to the
+    // end of the skeleton hierarchy as the squared distance from the skeleton to
+    // the nearest points in the point cloud.
 
+    const Particle* bestParticle = initialize();
+
+    float w = m_w0;
+
+    // run particle swarm optimization
+    for (int i = 0; i < m_numIterations; i++) {
+        for (int j = 0; j < m_numParticles; j++) {
+            Particle* particle = m_particles[j];
+
+            // update velocity
+            for (int k = 0; k < m_numVariables; k++) {
+                particle->v[k] = w * particle->v[k] +
+                        m_c1 * rand() * (particle->xStar[k] - particle->x[k]) +
+                        m_c2 * rand() * (bestParticle->xStar[k] - particle->x[k]);
+
+                if (particle->v[k] < -m_maxV)
+                    particle->v[k] = -m_maxV;
+                else if (particle->v[k] > m_maxV)
+                    particle->v[k] = m_maxV;
+
+                particle->x[k] += particle->v[k];
+            }
+
+            particle->evaluate();
+
+            if (particle->f < particle->pBest) {
+                if (particle->f < bestParticle->pBest)
+                    bestParticle = particle;
+                particle->updatePBest();
+            }
+        }
+
+        w -= (m_w0 - m_wt) / m_numIterations;
+    }
+}
+
+const FittingMethodPSO::Particle* FittingMethodPSO::initialize()
+{
+    Particle* bestParticle = 0;
+
+    for (size_t i = 0; i < m_particles.size(); i++) {
+        Particle* particle = m_particles[i];
+
+        // NOTE: problem dependent
+        for (int j = 0; j < m_numVariables; j++) {
+            particle->x[j] = (float)rand();
+            particle->v[j] = 0.0f;
+        }
+
+        particle->evaluate();
+        particle->updatePBest();
+
+        if (!bestParticle || particle->f < bestParticle->f)
+            bestParticle = particle;
+    }
+
+    return bestParticle;
 }
 }
--- src/tracking/fittingmethodpso.h
+++ src/tracking/fittingmethodpso.h
@@ -3,11 +3,14 @@
 
 #include <memory>
 #include <opencv2/opencv.hpp>
+#include <vector>
 
 #include "fittingmethod.h"
 
 namespace pose
 {
+
+// see http://www.ints.info.hiroshima-cu.ac.jp/~takahama/download/PSO.html
 
 // particle swarm optimization
 class FittingMethodPSO
@@ -23,6 +26,60 @@ protected:
                   std::shared_ptr<Skeleton> skeleton,
                   cv::Point3f centerOfMass,
                   const cv::Mat& projectionMatrix);
+
+private:
+    struct Particle
+    {
+        Particle(int numVar) {
+            numVar = numVar;
+            x = new float[numVar];
+            v = new float[numVar];
+            xStar = new float[numVar];
+            f = 0;
+            pBest = 0;
+        }
+
+        ~Particle() {
+            delete[] x;
+            delete[] v;
+            delete[] xStar;
+        }
+
+        int numVar;     // number of variables
+        float* x;       // state
+        float* v;       // velocity
+        float f;        // function to evaluate
+        float pBest;
+        float* xStar;
+
+        void evaluate() {
+            f = 0;
+            for (int i = 0; i < numVar; i++)
+                f += (x[i] - 1) * (x[i] - 1);
+        }
+
+        void updatePBest() {
+            for (int i = 0; i < numVar; i++)
+                xStar[i] = x[i];
+            pBest = f;
+        }
+    };
+
+    const Particle* initialize();
+
+    std::vector<Particle*> m_particles;
+
+    int m_numParticles;
+    int m_numIterations;
+    const int m_numVariables;
+
+    // The value of inertia weight at t=0 (W_0) and t=T_MAX (W_T)
+    const float m_w0;
+    const float m_wt;
+    const float m_maxV;
+    // The cognitive parameter (c1) and the social parameter (c2)
+    const float m_c1;
+    const float m_c2;
 };
 }
 
